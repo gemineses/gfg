1487f478478bff0022db76e4c7f21337b8ee872b
nthonygcib <ajuarez@gcib.net>
2015-09-15 16:30:15 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/material.min.css">
+		<script src="js/material.min.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	</head>
+	<body>
+		<!-- Colored FAB button -->
+		<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+		  <i class="material-icons">add</i>
+		</button>
+	</body>
+</html>
