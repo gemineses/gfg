e7b0fe470ae06e9f209ae0444c7394e73dce0eac
root <ajuarez@gcib.net>
2015-09-21 17:10:10 -0700

    core init

--- mvc/core/init.php
+++ mvc/core/init.php
@@ -1,5 +1,15 @@
 <?php
+	/*reading basic libraries*/
+
+	/*reading logs system*/
 	include('logs.php');
+	/*init a new conection to host*/
 	printlog('new_conection'."\n");
+	/*init routes system*/
 	include('../config/routes.php');
+	/*reading basics tags system*/
+	include('../config/maintags.php');
+	/*reading libraries(css, js, etc)*/
+	include('../config/libs.php');
+
 ?>
