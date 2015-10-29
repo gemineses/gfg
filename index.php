0ff94663bea2263c4c29a343bd18249bd8a5f89a
root <ajuarez@gcib.net>
2015-10-28 22:31:38 -0700

    trabajando en seccion para administrar las empresas

--- index.php
+++ index.php
@@ -7,46 +7,59 @@ session_start();
 	if($_SESSION['user']==''){
 		include('mvc/controller/main.php');
 	}else{
-		//TODO: encontrar una manera mas ordenada de casar estos urls
-		/**
-			Iniciando redireccionamiento
-		*/
-		$uri = $_SERVER['REQUEST_URI'];
+		if($_SESSION['compId']='1'){
+			$uri = $_SERVER['REQUEST_URI'];
+			$direct = substr($uri, 4);
 
-		$direct = substr($uri, 4);
-		/*Paginas generales*/
-		if($direct=="/"){
-			include('mvc/controller/menu.php');
-		}elseif($direct=="/menu"){
-			include('mvc/controller/menu.php');
-		}elseif($direct=="/sessionOff"){
-			session_destroy();
-			include('mvc/controller/main.php');
-		}elseif($direct=="/contabilidad"){
-			include('mvc/controller/contabilidad.php');
-		}elseif($direct=="/nomina"){
-			include('mvc/controller/nomina.php');
-		}elseif($direct=="/catalogo"){
-			include('mvc/controller/catalogo.php');
-		}elseif($direct=="/facturacion"){
-			include('mvc/controller/facturacion.php');
-		}elseif($direct=="/descargas"){
-			include('mvc/controller/descargas.php');
-		}
-		/*termina Paginas generales*/
+			if($direct=="/"){
+				include('mvc/controller/admin.php');
+			}elseif($direct=="/nuevaEmpresa"){
+				include('mvc/controller/adminNueva.php');
+			}elseif($direct=="/editEmpresa"){
+				include('mvc/controller/adminEdit.php');
+			}else{
+				include('mvc/controller/admin.php');
+			}
 
-		elseif(strpos($direct, "/catalogo") !== false){
-			include('mvc/controller/catalogo.php');
-		}
-		/*database functions, va y hace una consulta al DA que le pertenece*/
-		elseif(strpos($direct, "/da") !== false){
-			include('mvc/model/DA/dataAccess.php');
-		}
+		}else{
+			//TODO: encontrar una manera mas ordenada de casar estos urls
+			/**
+				Iniciando redireccionamiento
+			*/
+			$uri = $_SERVER['REQUEST_URI'];
 
-		else{
-			//echo "not file";
-		}	
+			$direct = substr($uri, 4);
+			/*Paginas generales*/
+			if($direct=="/"){
+				include('mvc/controller/menu.php');
+			}elseif($direct=="/menu"){
+				include('mvc/controller/menu.php');
+			}elseif($direct=="/sessionOff"){
+				session_destroy();
+				include('mvc/controller/main.php');
+			}elseif($direct=="/contabilidad"){
+				include('mvc/controller/contabilidad.php');
+			}elseif($direct=="/nomina"){
+				include('mvc/controller/nomina.php');
+			}elseif($direct=="/catalogo"){
+				include('mvc/controller/catalogo.php');
+			}elseif($direct=="/facturacion"){
+				include('mvc/controller/facturacion.php');
+			}elseif($direct=="/descargas"){
+				include('mvc/controller/descargas.php');
+			}
+			/*termina Paginas generales*/
+
+			elseif(strpos($direct, "/catalogo") !== false){
+				include('mvc/controller/catalogo.php');
+			}
+			/*database functions, va y hace una consulta al DA que le pertenece*/
+			elseif(strpos($direct, "/da") !== false){
+				include('mvc/model/DA/dataAccess.php');
+			}
+			else{
+				//echo "not file";
+			}
+		}
 	}
-	
-
 ?>
---
+++ mvc/controller/adminNueva.php
@@ -0,0 +1,18 @@
+<?php
+	$tags->docType();
+	$tags->html(true);
+	$tags->head(true);
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+	$libs->setLib('js', 'material.min');
+	$libs->setLib('js', 'angular.min');
+	$libs->setAngularModule('main', 'menu');
+	$libs->setLib('css', 'material.min');
+	$libs->setLib('css', 'main');
+	$libs->setPrincipalStyle('adminEmpresas');
+	echo '<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">';
+	$tags->head(false);
+	$tags->body(true);
+	include($routes->view.'adminNueva.php');
+	$tags->body(false);
+	$tags->html(false);
+?>
