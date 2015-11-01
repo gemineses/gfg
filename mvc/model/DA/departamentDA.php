86ec3e7d58d4d6e48d032da2e31f4b7d7171c81e
root <ajuarez@gcib.net>
2015-10-31 19:37:47 -0700

    departamentos, personas ya se puede agregar

--- mvc/model/DA/departamentDA.php
+++ mvc/model/DA/departamentDA.php
@@ -18,12 +18,19 @@
 		echo json_encode($arr);
 	}
 
+	function setNew(){
+		$sql = 'INSERT INTO DEPARTAMENT VALUES (0, "'.$_POST['txtName'].'")';
+		$result = mysql_query($sql);
+	}
+
 	//entra a funciones
 	$uri = $_SERVER['REQUEST_URI'];
 	$direct = substr($uri, 8);
 	
 	if(strpos($direct, "getAll") !== false){
 		getAll($conn);
+	}elseif(strpos($direct, "setNew") !== false){
+		setNew($conn);
 	}
 
 	//finaliza conexion
